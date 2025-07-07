5396dfd811037beddf79f79be6bd6699742e76b9
kyowanghwe <60578423+kyowanghwe@users.noreply.github.com>
2025-07-07 17:25:45 +0700

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,6 +15,6 @@ public class DemoApplication {
 
 	@GetMapping("/")
 	public String hello() {
-		return "Hello from Spring Boot on Kubernetes! 1111111";
+		return "Hello from Spring Boot on Kubernetes! test";
 	}
 }
